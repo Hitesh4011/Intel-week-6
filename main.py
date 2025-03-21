6d76875fd48b401d4aa20b83eb6b73f92650adbe
Hitesh4011 <153000329+Hitesh4011@users.noreply.github.com>
2025-03-21 10:38:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 # Load the trained model from the same repository
 model_url = os.path.join(os.path.dirname(__file__), "xgboost_model.pkl")
 
+st.set_page_config(page_title="Estimate Delivery Time", layout="wide")
+
 @st.cache_resource()
 def load_model():
     with open(model_url, "rb") as file:
@@ -39,14 +41,14 @@ with st.container():
 
     # Button to predict estimated wait time
     if st.button("Calculate Estimated Time"):
-        # input_features = np.array([[
-        #     purchase_day, purchase_month, year, product_size_cm3,
-        #     product_weight_g, geolocation_state_customer,
-        #     geolocation_state_seller, distance
-        # ]])
+        input_features = np.array([[
+            purchase_day, purchase_month, year, product_size_cm3,
+            product_weight_g, geolocation_state_customer,
+            geolocation_state_seller, distance
+        ]])
         
-        # prediction = model.predict(input_features)
-        # estimated_days = round(prediction[0], 2)
+        prediction = model.predict(input_features)
+        estimated_days = round(prediction[0], 2)
         estimated_days = 0
         
         st.success(f"📅 Estimated Delivery Time: {estimated_days} days")
