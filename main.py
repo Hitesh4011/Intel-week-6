868b840c17f25906a6a18389e5fe319bcd32a7ab
Hitesh4011 <153000329+Hitesh4011@users.noreply.github.com>
2025-03-21 10:43:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,17 +26,20 @@ with st.container():
     col1, col2 = st.columns(2)
 
     with col1:
-        purchase_day = st.number_input("Purchase Day", min_value=1, max_value=31, value=15)
+        purchase_day = st.number_input("Purchase Day", min_value=1, max_value=7, value=15)
         purchase_month = st.number_input("Purchase Month", min_value=1, max_value=12, value=6)
-        year = st.number_input("Year", min_value=2000, max_value=2030, value=2025)
+        year = st.number_input("Year", min_value=2000, max_value=2025, value=2025)
+        geolocation_state_seller = st.number_input("Geolocation State (Seller)", min_value=0, max_value=26, value=20)
 
     with col2:
         product_size_cm3 = st.number_input("Product Size (cm³)", min_value=1, value=10000)
         product_weight_g = st.number_input("Product Weight (g)", min_value=1, value=500)
+        distance = st.number_input("Distance (km)", min_value=0.1, value=300.5)
+        geolocation_state_customer = st.number_input("Geolocation State (Customer)", min_value=0, max_value=26, value=10)
 
-    geolocation_state_customer = st.number_input("Geolocation State (Customer)", min_value=1, max_value=50, value=10)
-    geolocation_state_seller = st.number_input("Geolocation State (Seller)", min_value=1, max_value=50, value=20)
-    distance = st.number_input("Distance (km)", min_value=0.1, value=300.5)
+    
+    
+    
 
     # Button to predict estimated wait time
     if st.button("Calculate Estimated Time"):
